28c8ab6a480d7db645b8803c39da9072804e67ff
12345krishna <83201393+12345krishna@users.noreply.github.com>
2021-05-07 05:23:14 -0700

    Add files via upload

---
+++ pythonsum.py
@@ -0,0 +1,14 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
